3df84cfa0be6f038d3bc8489b9ea23cc6a29452e
Natan Cardoso <natansc73@gmail.com>
2023-03-06 01:43:06 -0300

    footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,8 @@ import icon2NumberImg from '../assets/icon2.png'
 import icon3NumberImg from '../assets/icon3.png'
 import icon4NumberImg from '../assets/icon4.png'
 import checkImg from '../assets/pricing-check.svg'
+import phoneImg from '../assets/phone-icon.svg'
+import mailImg from '../assets/envelope.svg'
 
 export default function Home() {
   return (
@@ -353,6 +355,83 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      <footer>
+        <div className="container">
+          <div className="row pt-90 pb-90 justify-content-center">
+            <div className="col-lg-3 col-sm-6 order-lg-1 order-2 d-flex justify-content-sm-start justify-content-start">
+              <div className="footer-items contact ">
+                <h3>Contacts</h3>
+                <div className="hotline mb-30">
+                  <div className="hotline-icon">
+                    <Image src={phoneImg} alt="" width={23} height={23} />
+                  </div>
+                  <div className="hotline-info">
+                    <h6 className="mb-10"><a href="tel:+8801761111456">+880 176 1111 456</a></h6>
+                    <h6><a href="tel:+8801701111000">+880 170 1111 000</a></h6>
+                  </div>
+                </div>
+                <div className="email mb-30">
+                  <div className="email-icon">
+                    <Image src={mailImg} alt="" width={23} height={23} />
+                  </div>
+                  <div className="email-info">
+                    <h6 className="mb-10"><a href="mailto:info@example.com">info@example.com</a></h6>
+                    <h6><a href="mailto:info@support.com">info@support.com</a></h6>
+                  </div>
+                </div>
+                <div className="email">
+                  <div className="email-icon">
+                    <Image src={mailImg} alt="" width={23} height={23} />
+                  </div>
+                  <div className="email-info">
+                    <h6 className="mb-10"><a>168/170, Avenue 01, Mirpur</a></h6>
+                    <h6><a>DOHS, Bangladesh</a></h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 d-flex align-items-center order-lg-2 order-1 justify-content-sm-center justify-content-start">
+              <div className="footer-items">
+                <h2>want <span>to keep</span><br />
+                  your pet in, <span>our center</span>?</h2>
+                <div className="book-btn">
+                  <a className="primary-btn1" href="contact.html">Book Now</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6 d-flex justify-content-sm-end justify-content-start order-3">
+              <div className="footer-items opening-time">
+                <h3>Opening Hours</h3>
+                <h6 className="mb-25">Mon - Fri: 9.00AM - 6.00PM</h6>
+                <h6 className="mb-25">Saturday: 9.00AM - 6.00PM</h6>
+                <h6>Sunday: Closed</h6>
+                <ul className="social-icons">
+                  <li><a href="https://www.facebook.com/"><i className="bx bxl-facebook"></i></a></li>
+                  <li><a href="https://twitter.com/"><i className="bx bxl-twitter"></i></a></li>
+                  <li><a href="https://www.pinterest.com/"><i className="bx bxl-pinterest-alt"></i></a></li>
+                  <li><a href="https://www.instagram.com/"><i className="bx bxl-instagram"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="row border-top">
+            <div className="col-lg-6">
+              <div className="copyright-area">
+                <p>© 2023 Scooby is Proudly Powered by <a href="https://www.egenslab.com/"> Egens Lab.</a></p>
+              </div>
+            </div>
+            <div className="col-lg-6 d-flex justify-content-lg-end justify-content-center">
+              <ul className="footer-btm-menu">
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms &amp; Conditions</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Help</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
