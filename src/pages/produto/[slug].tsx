261f7f0725db7a29f01ce717d9a1fa41964ba8d7
Natan Cardoso <natansc73@gmail.com>
2023-03-26 22:17:37 -0300

    Update [slug].tsx

--- src/pages/produto/[slug].tsx
+++ src/pages/produto/[slug].tsx
@@ -1,18 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Stripe from "stripe";
 import { stripe } from "../../lib/stripe";
 import { useRouter } from "next/router";
 import axios from "axios";
 import { useState } from "react";
-import { Disclosure } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import logoImg from '../../assets/alertapetlogo.svg'
-import placeholderImg from '../../assets/placeholder.svg'
 import phoneImg from '../../assets/phone-icon.svg'
 import mailImg from '../../assets/envelope.svg'
+import Nav from "@/components/nav";
 interface ProductProps {
   product: {
     id: string,
@@ -77,83 +73,7 @@ export default function Product({ product }: ProductProps) {
         <meta name="theme-color" content="#ffffff" />
       </Head>
 
-      <Disclosure as="nav" className="bg-[#001D23]">
-        {({ open }) => (
-          <>
-            <div className="mx-auto w-11/12 px-2 sm:px-6 lg:px-8">
-              <div className="relative flex h-16 items-center justify-between">
-                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                  {/* Mobile menu button*/}
-                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-
-                <div className="flex flex-1 items-center justify-center sm:items-stretch">
-                  <Link href="/" className="flex flex-shrink-0 items-center">
-                    <Image
-                      className="block h-8 w-auto lg:hidden"
-                      src={logoImg}
-                      alt="Your Company"
-                      width={429}
-                      height={83}
-                    />
-                    <Image
-                      className="hidden h-8 w-auto lg:block"
-                      src={logoImg}
-                      alt="Your Company"
-                      width={429}
-                      height={83}
-                    />
-                  </Link>
-                  <div className="hidden sm:ml-6 sm:block">
-                    <div className="flex space-x-4">
-                      {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className={classNames(
-                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                            'rounded-md px-3 py-2 text-sm font-medium'
-                          )}
-                          aria-current={item.current ? 'page' : undefined}
-                        >
-                          {item.name}
-                        </a>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div>
-
-            <Disclosure.Panel className="sm:hidden">
-              <div className="space-y-1 px-2 pt-2 pb-3">
-                {navigation.map((item) => (
-                  <Disclosure.Button
-                    key={item.name}
-                    as="a"
-                    href={item.href}
-                    className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'block rounded-md px-3 py-2 text-base font-medium'
-                    )}
-                    aria-current={item.current ? 'page' : undefined}
-                  >
-                    {item.name}
-                  </Disclosure.Button>
-                ))}
-              </div>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
+      <Nav />
 
       <div className="flex w-11/12 mx-auto gap-10 my-12">
         <div>
