9c5eaf1a6fd74da954b04de4ea70be95bf67e779
Natan Cardoso <natansc73@gmail.com>
2023-03-30 00:28:57 -0300

    Update whatsapp.tsx

--- src/components/whatsapp.tsx
+++ src/components/whatsapp.tsx
@@ -3,10 +3,10 @@ import whatsappImg from '../assets/whatsapp-svgrepo-com.svg'
 
 export default function WhatsApp() {
   return (
-    <div className="fixed right-6 bottom-10 z-50 ">
+    <a href="https://wa.me/5519982606755?text=Oi, tudo bem? Pode me ajudar a encontrar meu pet?" target="_blank" className="fixed right-6 bottom-10 z-50">
       <button className="bg-[#1ec725] p-5 rounded-full">
         <Image src={whatsappImg} alt="" height={26} width={26} />
       </button>
-    </div>
+    </a>
   )
 }
